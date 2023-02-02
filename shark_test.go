f8f0a072dc8eca8e8dd24457029145dd4c0d0c02
Lelo88 <villalbaleandrogabriel@gmail.com>
2023-02-02 19:43:46 -0300

    test sharkistired OK

--- shark_test.go
+++ shark_test.go
@@ -1,13 +1,26 @@
 package hunt
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSharkHuntsSuccessfully(t *testing.T) {
+	
+	
 }
 
 func TestSharkCannotHuntBecauseIsTired(t *testing.T) {
+	//arrange
+	shark := Shark{tired: true}
+	prey := Prey{}
+	IsTired := errors.New("cannot hunt, i am really tired")
+	//act 
+	err := shark.Hunt(&prey)
+	assert.Error(t, err)
+	assert.Equal(t,err,IsTired)
 }
 
 func TestSharkCannotHuntBecaisIsNotHungry(t *testing.T) {
